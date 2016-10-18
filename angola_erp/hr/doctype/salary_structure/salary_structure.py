c4cffe15c17ce8b0d3f463bc873d830b43314258
helkyd <hcesar@gmail.com>
2016-10-18 15:17:07 +0100

    Para ser copiado

---
+++ angola_erp/hr/doctype/salary_structure/salary_structure.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+# Helkyd V7.0.58
+
+from __future__ import unicode_literals
+import frappe
+
+from frappe.utils import cstr, flt, getdate
+from frappe.model.naming import make_autoname
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.document import Document
+from erpnext.hr.utils import set_employee_name
+
+class SalaryStructure(Document):
+	def autoname(self):
+		self.name = make_autoname(self.employee + '/.SST' + '/.#####')
+		
+	def validate(self):
+		self.check_overlap()
+		self.validate_amount()
+		self.validate_employee()
+		self.validate_joining_date()
+		set_employee_name(self)
+
+	def get_employee_details(self):
+		ret = {}
+		det = frappe.db.sql("""select employee_name, branch, designation, department
+			from `tabEmployee` where name = %s""", self.employee)
+		if det:
+			ret = {
+				'employee_name': cstr(det[0][0]),
+				'branch': cstr(det[0][1]),
+				'designation': cstr(det[0][2]),
+				'department': cstr(det[0][3]),
+				'backup_employee': cstr(self.employee)
+			}
+		return ret
+
+	def get_ss_values(self,employee):
+		basic_info = frappe.db.sql("""select bank_name, bank_ac_no
+			from `tabEmployee` where name =%s""", employee)
+		ret = {'bank_name': basic_info and basic_info[0][0] or '',
+			'bank_ac_no': basic_info and basic_info[0][1] or ''}
+		return ret
+
+	def make_table(self, doct_name, tab_fname, tab_name):
+		list1 = frappe.db.sql("select name,abono,desconto from `tab%s` where docstatus != 2" % doct_name)
+		for li in list1:
+			if(tab_fname == 'earnings' and li[1] == 1):
+				child = self.append(tab_fname, {})
+				child.salary_component = cstr(li[0])
+				child.amount = 0
+			elif(tab_fname == 'deductions' and li[2] == 1):
+				child = self.append(tab_fname, {})
+				child.salary_component = cstr(li[0])
+				child.amount = 0
+
+	def make_earn_ded_table(self):
+		self.make_table('Salary Component','earnings','Salary Detail')
+		self.make_table('Salary Component','deductions', 'Salary Detail')
+
+	def check_overlap(self):
+		existing = frappe.db.sql("""select name from `tabSalary Structure`
+			where employee = %(employee)s and
+			(
+				(%(from_date)s > from_date and %(from_date)s < to_date) or
+				(%(to_date)s > from_date and %(to_date)s < to_date) or
+				(%(from_date)s <= from_date and %(to_date)s >= to_date))
+			and name!=%(name)s
+			and docstatus < 2""",
+			{
+				"employee": self.employee,
+				"from_date": self.from_date,
+				"to_date": self.to_date,
+				"name": self.name or "No Name"
+			}, as_dict=True)
+			
+		if existing:
+			frappe.throw(_("Salary structure {0} already exist, more than one salary structure for same period is not allowed").format(existing[0].name))
+
+	def validate_amount(self):
+		if flt(self.net_pay) < 0 and self.salary_slip_based_on_timesheet:
+			frappe.throw(_("Net pay cannot be negative"))
+
+	def validate_employee(self):
+		old_employee = frappe.db.get_value("Salary Structure", self.name, "employee")
+		if old_employee and self.employee != old_employee:
+			frappe.throw(_("Employee can not be changed"))
+
+	def validate_joining_date(self):
+		joining_date = getdate(frappe.db.get_value("Employee", self.employee, "date_of_joining"))
+		if getdate(self.from_date) < joining_date:
+			frappe.throw(_("From Date in Salary Structure cannot be lesser than Employee Joining Date."))
+
+@frappe.whitelist()
+def make_salary_slip(source_name, target_doc=None):
+	def postprocess(source, target):
+		# copy earnings and deductions table
+		for key in ('earnings', 'deductions'):
+			for d in source.get(key):
+				target.append(key, {
+					'amount': d.amount,
+					'default_amount': d.amount,
+					'depends_on_lwp' : d.depends_on_lwp,
+					'salary_component' : d.salary_component
+				})
+
+		target.run_method("pull_emp_details")
+		target.run_method("get_leave_details")
+		target.run_method("calculate_net_pay")
+			
+
+	doc = get_mapped_doc("Salary Structure", source_name, {
+		"Salary Structure": {
+			"doctype": "Salary Slip",
+			"field_map": {
+				"total_earning": "gross_pay",
+				"name": "salary_structure"
+			}
+		}
+	}, target_doc, postprocess, ignore_child_tables=True)
+
+	return doc
