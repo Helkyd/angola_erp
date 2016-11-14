5b86faa2087f14d39e07d08c8872f93543a03eb1
helio <hcesar@gmail.com>
2016-11-14 09:14:40 +0100

    Modificacoes no Sales Order vendo que foi uma copia do Rigl

--- angola_erp/fixtures/custom_scripts/Sales Order.js
+++ angola_erp/fixtures/custom_scripts/Sales Order.js
@@ -1,11 +1,11 @@
-cur_frm.add_fetch('customer','payment_terms','payment_terms');
-cur_frm.add_fetch('customer','letter_head','letter_head');
-cur_frm.add_fetch('shipping_address_name','address_title','shipping_address_title');
-cur_frm.add_fetch('shipping_address_name','tin_no','shipping_tin_no');
-cur_frm.add_fetch('shipping_address_name','excise_no','shipping_excise_no');
-cur_frm.add_fetch('customer_address','excise_no','excise_number');
-cur_frm.add_fetch('customer_address','tin_no','tin_no');
-cur_frm.add_fetch('taxes_and_charges','letter_head','letter_head');
+//cur_frm.add_fetch('customer','payment_terms','payment_terms');
+//cur_frm.add_fetch('customer','letter_head','letter_head');
+//cur_frm.add_fetch('shipping_address_name','address_title','shipping_address_title');
+//cur_frm.add_fetch('shipping_address_name','tin_no','shipping_tin_no');
+//cur_frm.add_fetch('shipping_address_name','excise_no','shipping_excise_no');
+//cur_frm.add_fetch('customer_address','excise_no','excise_number');
+//cur_frm.add_fetch('customer_address','tin_no','tin_no');
+//cur_frm.add_fetch('taxes_and_charges','letter_head','letter_head');
 
 cur_frm.cscript.custom_validate = function(doc, dt, dn) {
     if (doc.docstatus == 0){
