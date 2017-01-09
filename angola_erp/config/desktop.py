e6874ec325d279aa7af01af0e9e49e35ea9dd556
helio <hcesar@gmail.com>
2017-01-09 22:42:16 +0100

    Correcao codigo API verificacao Check_out Acresciom Recibo_fatura_1 (copy from POS receipt)

--- angola_erp/config/desktop.py
+++ angola_erp/config/desktop.py
@@ -13,11 +13,19 @@ def get_data():
 		},
 
 		{
-			"module_name": "gestao residencial",
+			"module_name": "Gestao Residencial",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Gestao Residencial")
+		},
+		{
+			"module_name": "Bar e Restaurante",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Bar e Restaurante")
 		}
+
 
 	]
