2e800d0ee22118566bff62945b44321e8a1108bf
helio <hcesar@gmail.com>
2017-06-08 17:36:01 +0100

    Estrutura IRT changed due to calculations of IRT, INSS and others ... Added some fields like Incidencia so that later this can be checked what itens are calculated or need to be calculated with INSS and IRT Removed the second Salary process from the refresh (duplicate maybe)... Salary Compenents added to facilitate IRT calculation. Still need to calculate INSS %8

--- angola_erp/angola_erpnext/validations/irt.py
+++ angola_erp/angola_erpnext/validations/irt.py
@@ -98,7 +98,7 @@ def get_faltas(emp,mes,ano, empresa):
 	print " DADOS ATTENDANCE"
 	print emp, ' ', mes, ' ', ano
 	j= frappe.db.sql(""" SELECT count(status)
-	from `tabAttendance` where employee = %s and status = 'Absent' and month(att_date) = %s and year(att_date) = %s and company = %s and docstatus=1 """,(emp,mes,ano, empresa), as_dict=False)
+	from `tabAttendance` where employee = %s and status = 'Absent' and month(attendance_date) = %s and year(attendance_date) = %s and company = %s and docstatus=1 """,(emp,mes,ano, empresa), as_dict=False)
 
 	print " ATTENDANCE"
 	print j[0][0]	
