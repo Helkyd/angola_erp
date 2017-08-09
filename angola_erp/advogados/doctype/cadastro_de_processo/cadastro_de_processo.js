b2d0330729f7a10852843465be463f940ab9f91d
helio <hcesar@gmail.com>
2017-08-09 16:23:39 +0100

    Changed Serial to single fiel selection Numero processo is generated based on the number given and join with Serial field to form the Process Number

--- angola_erp/advogados/doctype/cadastro_de_processo/cadastro_de_processo.js
+++ angola_erp/advogados/doctype/cadastro_de_processo/cadastro_de_processo.js
@@ -26,6 +26,41 @@ frappe.ui.form.on('Cadastro de Processo', {
 		}
 
 
+	},
+	validate: function(frm){
+		//Check numero processo is Number and 4 digits.
+		if (cur_frm.doc.numero_de_processo.length < 4){
+			if (cur_frm.doc.numero_de_processo.length == 1){
+				frappe.model.set_value(cdt,cdn,'numero_de_processo','000' + cur_frm.doc.numero_de_processo)
+			}else if (cur_frm.doc.numero_de_processo.length == 2){
+				frappe.model.set_value(cdt,cdn,'numero_de_processo','00' + cur_frm.doc.numero_de_processo)
+			}else if (cur_frm.doc.numero_de_processo.length == 3){
+				frappe.model.set_value(cdt,cdn,'numero_de_processo','0' + cur_frm.doc.numero_de_processo)
+			}
+			console.log(cur_frm.doc.numero_de_processo + cur_frm.doc.naming_series)
+			validation = false
+	
+		}else if (cur_frm.doc.numero_de_processo.length > 4){
+			if (cur_frm.doc.numero_de_processo.length != 16){
+				alert('Numero de Processo tem que ter somente 4 digitos')
+				frappe.model.set_value(cdt,cdn,'numero_de_processo',null)
+				validation = false
+			}
+		}else if (cur_frm.doc.numero_de_processo == '0000'){
+			alert('Numero de Processo nao pode ser 0000')
+			frappe.model.set_value(cdt,cdn,'numero_de_processo',null)
+			validation = false
+
+		}
+		if (isNaN(cur_frm.doc.numero_de_processo)){
+			if (cur_frm.doc.numero_de_processo.length != 16){
+				alert('Numero de Processo tem que ter somente digitos')
+				frappe.model.set_value(cdt,cdn,'numero_de_processo',0)
+				validation = false
+			}
+		}
+
+
 	}
 });
 
@@ -38,6 +73,32 @@ frappe.ui.form.on('Cadastro de Processo','nome_do_advogado',function(frm,cdt,cdn
 frappe.ui.form.on('Cadastro de Processo','tipo_de_processo',function(frm,cdt,cdn){
 
 	categoria_('Tipo de Processo',cur_frm.doc.tipo_de_processo)
+});
+
+frappe.ui.form.on('Cadastro de Processo','nome_do_autor',function(frm,cdt,cdn){
+	//Check numero processo is Number and 4 digits.
+
+	if (cur_frm.doc.numero_de_processo.length < 4){
+		if (cur_frm.doc.numero_de_processo.length == 1){
+			frappe.model.set_value(cdt,cdn,'numero_de_processo','000' + cur_frm.doc.numero_de_processo)
+		}else if (cur_frm.doc.numero_de_processo.length == 2){
+			frappe.model.set_value(cdt,cdn,'numero_de_processo','00' + cur_frm.doc.numero_de_processo)
+		}else if (cur_frm.doc.numero_de_processo.length == 3){
+			frappe.model.set_value(cdt,cdn,'numero_de_processo','0' + cur_frm.doc.numero_de_processo)
+		}
+		console.log(cur_frm.doc.numero_de_processo + cur_frm.doc.naming_series)
+
+	
+	}else if (cur_frm.doc.numero_de_processo.length > 4){
+		if (cur_frm.doc.numero_de_processo.length != 16){
+			alert('Numero de Processo tem que ter somente 4 digitos')
+			frappe.model.set_value(cdt,cdn,'numero_de_processo',null)
+		}
+	}else if (cur_frm.doc.numero_de_processo == '0000'){
+		alert('Numero de Processo nao pode ser 0000')
+		frappe.model.set_value(cdt,cdn,'numero_de_processo',null)
+
+	}
 });
 
 
