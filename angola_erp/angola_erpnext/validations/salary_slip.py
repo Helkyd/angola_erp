c41a45c0e8cd9bbbb7beb226c05ea8fb9885f9ec
helio <hcesar@gmail.com>
2017-10-28 01:16:16 +0100

    Correcao due to a None or empty string

--- angola_erp/angola_erpnext/validations/salary_slip.py
+++ angola_erp/angola_erpnext/validations/salary_slip.py
@@ -507,7 +507,7 @@ def valida_sub_ferias(doc,dias_pagamento,total_dias_trabalho):
 		join `tabSalary Detail` as sd on sd.parent = se.parent where se.employee = %s and sd.abbr = %s """,(emp.name,d.abbr),as_dict=True)
 		qry_result=''
 		
-		if len(qry_cmpnt):
+		if (qry_cmpnt):
 			if qry_cmpnt[0].formula !='': 
 				print '1 ',qry_cmpnt[0].formula
 				qry_result = qry_cmpnt[0].formula
