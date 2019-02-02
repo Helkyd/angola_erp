96cf72f446d4ce3f71a96a92ae94c7f984844006
helio <hcesar@gmail.com>
2019-02-02 09:25:43 +0100

    Created a report for IPC without reflecting on Accounts entry Some other fixes...

---
+++ angola_erp/angola_erpnext/report/imposto_de_consumo/imposto_de_consumo.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2016, Helio de Jesus and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+
+frappe.query_reports["Imposto de Consumo"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		}
+
+	]
+}
