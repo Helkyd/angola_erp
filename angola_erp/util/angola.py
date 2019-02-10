d670f2136744b5710e88ffb2b212834f402d1804
helio <hcesar@gmail.com>
2019-02-10 13:39:00 +0100

    Fix and improve DN+ to able to search the DN to be added...

--- angola_erp/util/angola.py
+++ angola_erp/util/angola.py
@@ -917,10 +917,14 @@ def none(source_name, target_doc=None):
 	return source_name
 
 @frappe.whitelist()
-def get_dn_for_si(source_name):
+def get_dn_for_si(source_name=None):
 	#source_name should be customer name
 
-	dn_for_si =  frappe.model.frappe.get_all('Delivery Note',filters={'customer':source_name,'docstatus':1,'status':'To Bill'},fields=['name','customer','posting_date'])
+	if source_name == None:
+		dn_for_si =  frappe.model.frappe.get_all('Delivery Note',filters={'docstatus':1,'status':'To Bill'},fields=['name','customer','posting_date'])
+
+	else:
+		dn_for_si =  frappe.model.frappe.get_all('Delivery Note',filters={'customer':source_name,'docstatus':1,'status':'To Bill'},fields=['name','customer','posting_date'])
 
 	
 	return dn_for_si
