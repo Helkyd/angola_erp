a4d14c3e7b1b34b7b642ee2e0a1a1a035e205e8d
helio <hcesar@gmail.com>
2019-02-10 12:22:27 +0100

    Fix DN for SI status to be Bill

--- angola_erp/util/angola.py
+++ angola_erp/util/angola.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2016, Helio de Jesus and contributors
 # For license information, please see license.txt
 
-#Date Changed: 12/01/2019
+#Date Changed: 10/02/2019
 
 from __future__ import unicode_literals
 
@@ -920,7 +920,7 @@ def none(source_name, target_doc=None):
 def get_dn_for_si(source_name):
 	#source_name should be customer name
 
-	dn_for_si =  frappe.model.frappe.get_all('Delivery Note',filters={'customer':source_name,'docstatus':1},fields=['name','customer','posting_date'])
+	dn_for_si =  frappe.model.frappe.get_all('Delivery Note',filters={'customer':source_name,'docstatus':1,'status':'To Bill'},fields=['name','customer','posting_date'])
 
 	
 	return dn_for_si
