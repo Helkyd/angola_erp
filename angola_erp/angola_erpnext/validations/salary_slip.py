f569ab39980e6fcf4441e7dacde2b4d4e9f34dd7
helio <hcesar@gmail.com>
2019-06-19 22:18:34 +0100

    Returned to old ..the Salary Process fix..

--- angola_erp/angola_erpnext/validations/salary_slip.py
+++ angola_erp/angola_erpnext/validations/salary_slip.py
@@ -551,7 +551,7 @@ def proc_salario_iliquido(mes,ano,empresa):
 		#SI = (SB + HE - PA + PP) - (FTJSS - FI )
 		print salslip
 		salario_iliquido =0;
-		tab_detalhes = frappe.db.sql(""" select parent,abbr,amount from `tabSalary Detail` where abbr in ('SB','HE','PA','PP','FTJSS','FI','IH','SDF','DU','ST','ABF','SA','PAT','SN','FTI1') and parent= %s""",(salslip.name),as_dict=True)
+		tab_detalhes = frappe.db.sql(""" select parent,abbr,amount from `tabSalary Detail` where abbr in ('SB','HE','PA','PP','FTJSS','FI','IH','SDF','DU','ST','ABF','SA','PAT','SN') and parent= %s""",(salslip.name),as_dict=True)
 
 #(SB + HE + PA + PP + IH + SDF + DU + ST) - (FTJSS - FTI1)
 
@@ -563,7 +563,7 @@ def proc_salario_iliquido(mes,ano,empresa):
 #			elif (r.abbr == 'SA') or (r.abbr == 'ABF'):
 #				salario_iliquido = salario_iliquido - flt(r.amount)
 
-			elif (r.abbr == 'FTJSS') or (r.abbr == 'FI') or (r.abbr == 'FTI1'):
+			elif (r.abbr == 'FTJSS') or (r.abbr == 'FI'): #or (r.abbr == 'FTI1'):
 				salario_iliquido = salario_iliquido - flt(r.amount)
 
 			print "iliquido"
