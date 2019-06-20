41b5be8d995e4401dd933aa366e57b7123d682de
helio <hcesar@gmail.com>
2019-06-20 18:20:42 +0100

    Two new reports created per customer request... New Version v0.3.08

---
+++ angola_erp/angola_erpnext/report/user_item_wise_sales_register_summary/user_item_wise_sales_register_summary.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2016, Helio de Jesus and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["User Item-wise Sales Register Summary"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname":"customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"mode_of_payment",
+			"label": __("Mode of Payment"),
+			"fieldtype": "Link",
+			"options": "Mode of Payment"
+		},
+		{
+			"fieldname":"owner",
+			"label": __("User"),
+			"fieldtype": "Link",
+			"options": "User",
+			"default": frappe.session.user !='Administrator' && frappe.session.user 
+		}
+
+	]
+}
